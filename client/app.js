57adc382a59129e27610b65db6b365c9052aca26
Casey <casey.moreno0012@gmail.com>
2022-08-28 19:26:48 -0400

    styling

--- client/app.js
+++ client/app.js
@@ -12,15 +12,14 @@ function getBathValue() {
   
   function onClickedEstimatePrice() {
     console.log("Estimate price button clicked");
-    var sqft = document.getElementById("uiSqft");
-    var beds = getBedValue();
-    var bathrooms = getBathValue();
-    var location = document.getElementById("uiLocations");
-    var estPrice = document.getElementById("uiEstimatedPrice");
+    const sqft = document.getElementById("uiSqft");
+    const beds = getBedValue();
+    const bathrooms = getBathValue();
+    const location = document.getElementById("uiLocations");
+    const estPrice = document.getElementById("uiEstimatedPrice");
     console.log(`Datatype Bath ${typeof(bathrooms)}`)
-    var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-    //var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-  
+    const url = "http://127.0.0.1:5000/predict_home_price"; 
+
     $.post(url, {
         sq_feet: parseFloat(sqft.value),
         location: location.value,
@@ -37,8 +36,7 @@ function getBathValue() {
   function onPageLoad() {
     console.log( "document loaded" );
 
-    var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-    //var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    const url = "http://127.0.0.1:5000/get_location_names"; 
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
         if(data) {
